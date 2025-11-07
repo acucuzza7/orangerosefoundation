1bded41428e91f0994b33cf2b3401acf5d97c31c
acucuzza7 <anthony.cucuzza1@gmail.com>
2025-11-07 09:18:06 -0500

    halfway

--- multimedia/js/app.js
+++ multimedia/js/app.js
@@ -1,28 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const logoImg = document.getElementById("nav-logo-img");
 
-  // Define your logo image paths
-  const desktopLogo = "https://img1.wsimg.com/isteam/ip/163be76b-063f-4dc3-89dc-56d7cddae036/Logo%20website.png";
-  const mobileLogo = "multimedia/images/orange_rose.png";
-
-  function updateLogo() {
-    if (window.innerWidth <= 768) {
-      // Mobile
-      logoImg.src = mobileLogo;
-      logoImg.style.display = "block";
-    } else {
-      // Desktop
-      logoImg.src = desktopLogo;
-      logoImg.style.display = "block";
-    }
-  }
-
-  // Run on page load
-  updateLogo();
-
-  // Update on resize
-  window.addEventListener("resize", updateLogo);
-});
 
 
 
